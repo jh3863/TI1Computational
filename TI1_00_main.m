47b25af7452cc2b3a67ff5b11eb74ab939704fa9
jh3863 <jh3863@nyu.edu>
2023-11-12 14:27:09 -0600

    Add Q2c

--- TI1_00_main.m
+++ TI1_00_main.m
@@ -112,22 +112,23 @@ saveas(gcf,'plots/Q2a.png');
 %% %%% 01B Delta = 0.5, gN=500 %%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-gN   = 2000; Vhistory = nan(gN,iterMax);
 % define depreciation rate
 delta= 0.5;
 
-% update k grid
-kbar = [0.1 (z./(delta))^(1/(1-alpha)) ];
-gridK= [kbar(1):(kbar(2)-kbar(1))/(gN-1):kbar(2)]'
-
 % define C for a given k and k' using the budget constraint
 cGivenKK = @(k,kprime) z.*k.^alpha + (1-delta).*k - kprime
+
 
 % theoretical steady state values
 theoryK_steadystate = (alpha*z*beta/(1-beta+beta*delta))^(1/(1-alpha));
 theoryC_steadystate = z*theoryK_steadystate^alpha-delta*theoryK_steadystate;
 
-%(alpha*z*beta/(1-beta+beta*delta))^(1/(1-alpha));
+
+% update k grid
+gN   = 500; Vhistory = nan(gN,iterMax);
+kbar = [0.1 (z./(delta))^(1/(1-alpha)) ];
+gridK= [kbar(1):(kbar(2)-kbar(1))/(gN-1):kbar(2)]'
+
 
 % initialize value function iteration (VFI)
 Vdiff=10; iter=0; 
@@ -139,6 +140,7 @@ V = 0.* gridK;
 Vhistory(:,1) = V; VdiffH = nan(1,iterMax);
 
 % run VFI
+tic
 while Vdiff>tol & iter <iterMax
     cfeasible = cGivenKK(gridK,gridK')>=0;
     c = cGivenKK(gridK,gridK') .* cfeasible;
@@ -148,6 +150,7 @@ while Vdiff>tol & iter <iterMax
     % store history
     VdiffH(iter) = Vdiff./tol; Vhistory(:,iter) = V;
 end
+toc
 VdiffH(iter-5:iter+1)
 iter
 
@@ -165,11 +168,90 @@ legend([{'VFI'} {'45°'} ],'Location','southeast');
 
 saveas(gcf,'plots/Q2b.png');
 
-
 [gridK gridK(index) gridK-gridK(index)]
 
 [~,index_steadyK]= min(abs(gridK-gridK(index)));
-kplus = gridK(index); K_steadystate = kplus(index_steadyK)
-[K_steadystate theoryK_steadystate]
+kplus = gridK(index); K_steadystate = kplus(index_steadyK);
+K_ss_diff = K_steadystate-theoryK_steadystate;
+[K_steadystate theoryK_steadystate  K_ss_diff K_ss_diff/theoryK_steadystate]
 
-[cGivenKK(K_steadystate,K_steadystate) theoryC_steadystate]
+C_ss_diff = cGivenKK(K_steadystate,K_steadystate)-theoryC_steadystate;
+[cGivenKK(K_steadystate,K_steadystate) theoryC_steadystate C_ss_diff C_ss_diff/theoryC_steadystate]
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% %%% 01C Delta = 0.5,Loop over grid sizes %%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% define grid sizes
+gNgrid = [50, 100, 500, 1000, 2000];
+gNgridLength = size(gNgrid,2);
+
+gNgridTime = nan(1,gNgridLength);
+gNgridKss  = nan(1,gNgridLength);
+gNgridCss  = nan(1,gNgridLength);
+for iNgrid=1:gNgridLength
+    gN   = gNgrid(iNgrid); 
+
+    % update k grid
+    Vhistory = nan(gN,iterMax);
+    kbar = [0.1 (z./(delta))^(1/(1-alpha)) ];
+    gridK= [kbar(1):(kbar(2)-kbar(1))/(gN-1):kbar(2)]';
+    
+    
+    % initialize value function iteration (VFI)
+    Vdiff=10; iter=0; 
+    
+    % define initial value function guess
+    V = 0.* gridK;
+    
+    % define objects to track v_n at each iteration
+    Vhistory(:,1) = V; VdiffH = nan(1,iterMax);
+    
+    % run VFI
+    tic
+    while Vdiff>tol & iter <iterMax
+        cfeasible = cGivenKK(gridK,gridK')>=0;
+        c = cGivenKK(gridK,gridK') .* cfeasible;
+        [Vnew , index] = max( u(c) + beta* V',[], 2   );
+        Vdiff = sum((Vnew- V).^2); V= Vnew; iter = iter+1;
+    
+        % store history
+        VdiffH(iter) = Vdiff./tol; Vhistory(:,iter) = V;
+    end
+    gNgridTime(iNgrid) = toc
+
+    % extract steady state values
+    [~,index_steadyK]= min(abs(gridK-gridK(index)));
+    kplus = gridK(index); gNgridKss(iNgrid) = kplus(index_steadyK);
+
+    gNgridCss(iNgrid) = cGivenKK(gNgridKss(iNgrid),gNgridKss(iNgrid));
+
+end
+
+gNgridTime
+gNgridKss
+gNgridCss
+
+figure; set(gcf,'position',[400,800,1500,600]);
+p =subplot(1,3,1);
+%plot(gNgrid, gNgridTime,'O-', 'LineWidth',4); grid on; hold on; ylabel('time'); xlabel('grid size');
+plot(gNgrid, gNgridTime,'O-', 'LineWidth',4); grid on; hold on; ylabel('time (in seconds)'); xlabel('grid size');
+title('A) Computational Time'); p.FontSize =20;
+
+
+p = subplot(1,3,2);
+plot(gNgrid, gNgridKss , 'bO-', 'LineWidth',4); grid on; hold on; ylabel('k_{ss} & c_{ss}'); xlabel('grid size');
+plot(gNgrid, ones(1,gNgridLength)*theoryK_steadystate , 'k--', 'LineWidth',2); 
+plot(gNgrid, gNgridCss , 'rO-', 'LineWidth',4); grid on; hold on;
+plot(gNgrid, ones(1,gNgridLength)*theoryC_steadystate , 'k--', 'LineWidth',2); 
+title('B) Steady state comparison'); p.FontSize =20;
+%legend([{'k_{ss}'} {''} {'c_{ss}'} {''}],'Location','southeast');
+
+p = subplot(1,3,3);
+plot(gNgrid, abs(gNgridKss-theoryK_steadystate), 'bO-', 'LineWidth',4); grid on; hold on; ylabel('|x_{ss}-x_{ss}^T|'); xlabel('grid size');
+plot(gNgrid, abs(gNgridCss-theoryC_steadystate), 'rO-', 'LineWidth',4); grid on; hold on;
+title('C) Steady state absolute difference'); p.FontSize =20;
+legend([{'k_{ss}'} {'c_{ss}'}],'Location','southeast');
+
+saveas(gcf,'plots/Q2c.png');
